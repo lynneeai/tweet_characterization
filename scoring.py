62a5bd41bf8b9bfae1dc4c4855bd656c713aa778
Lin Ai (Columbia) <la2734@columbia.edu>
2022-02-16 14:37:15 -0500

    eval2 data

--- scoring.py
+++ scoring.py
@@ -1,6 +1,21 @@
-import json
+import csv
+import os
 import torch
 import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import classification_report
+from models import CLIP_MODEL, ModelWithTemperature
+from semafor_training.load_data import load_dataloaders
+from calibrate_config import CALIBRATE_CONFIG
+
+
+INTENT_TO_INTENTLABEL = {
+    "POLAR": "To polarize", 
+    "CALL_TO_ACTION": "To call to action", 
+    "VIRAL": "To become viral", 
+    "SARCASM": "To amuse", 
+    "HUMOR": "To amuse"
+}
 
 
 def softmax(x, axis):
@@ -28,34 +43,61 @@ def predict_label(llr, threshold):
     return 0 if llr < threshold else 1
 
 
-def inference(model, image_file, text, threshold):
+def inference(model, image_file, text, threshold=0):
     model.eval()
-    output = model([image_file], [text[:77]])[0].data.cpu().numpy()
+    output, _, intent_outputs_dict = model([image_file], [text])
+    output = output.data.cpu().numpy()
+    
     scores = softmax(output, axis=1)
     llr_dict = LLR(scores[0])
     predict = predict_label(llr_dict["logLikelihoodRatio"], threshold)
     
-    with open("LLR.jsonl", "w") as outfile:
-        json.dump(str(llr_dict), outfile)
-        outfile.write("\n")
+    intents = []
+    for intent in INTENT_TO_INTENTLABEL:
+        intent_predict = torch.max(intent_outputs_dict[intent], 1)[1]
+        intent_predict = intent_predict.data.cpu().numpy().tolist()[0]
+        if intent_predict == 1:
+            intents.append(INTENT_TO_INTENTLABEL[intent])
     
-    return llr_dict, predict
-
-
-def read_AOM(json_file):
-    with open(json_file, "r") as infile:
-        aom = json.load(infile)
-        image_file = aom["image_file"]
-        text = aom["text"]
-    return image_file, text
+    return llr_dict, predict, intents
 
 
 if __name__ == "__main__":
     
-    model = torch.load("./trained_models/covid19+climate_calibrated/clip_multi.pth")
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    clip_model = CLIP_MODEL(device=device, output_size=2).to(device)
+    intent_model = ModelWithTemperature(clip_model, device=device).to(device)
+    intent_model.load_state_dict(torch.load(
+        "./trained_models/covid19+climate_calibrated/clip_multi.weights.best", 
+        map_location=device
+    ))
     
-    image_file, text = read_AOM("test.json")
-    llr_dict, label = inference(model, image_file, text, 0)
-    print(llr_dict)
-    print(label)
+    labels, preds = [], []
+    tids, intents_list = [], []
+    with open("./processed_data/eval2/tweets.tsv", "r") as infile:
+        tsv_reader = csv.DictReader(infile, delimiter="\t")
+        pbar = tqdm(total=60)
+        for row in tsv_reader:
+            labels.append(int(row["label"]))
+            _, predict, intents = inference(intent_model, row["image_file"], row["text"])
+            preds.append(predict)
+            tids.append(row["tid"])
+            intents_list.append(intents)
+            pbar.update(1)
+        pbar.close()
     
+    if not os.path.exists("./results/eval2"):
+        os.makedirs("./results/eval2")
+    class_report_str = classification_report(labels, preds, target_names=["BENIGN", "MALICIOUS"], digits=5)
+    with open("./results/eval2/class_report.txt", "w") as outfile:
+        outfile.write(class_report_str)
+    with open("./results/eval2/preds.tsv", "w") as outfile:
+        tsv_writer = csv.DictWriter(outfile, fieldnames=["tid", "label", "pred", "intents"], delimiter="\t")
+        tsv_writer.writeheader()
+        for idx, tid in enumerate(tids):
+            tsv_writer.writerow({
+                "tid": tid,
+                "label": labels[idx],
+                "pred": preds[idx],
+                "intents": intents_list[idx]
+            })
